c0056c018c6dd3f831ccb13303b7226ea52b444d
Skar <129614348+SkarSys@users.noreply.github.com>
2023-12-10 21:13:55 +0000

    Update offsets.h
    Updated offsets to newest patch (10th of december 2023)

--- um/offsets.h
+++ um/offsets.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Windows.h>
 
-#define OFFSET_UWORLD 0x10AEC0B8
+#define OFFSET_UWORLD 0x117392A8
 #define VIEW_POINT 0xE49C7E0
 
 namespace OFFSETS
@@ -10,13 +10,13 @@ namespace OFFSETS
     uintptr_t CurrentVehicle = 0x25B8;
     uintptr_t CachedFuelComponent = 0x10F8;
     uintptr_t bUseGravity = 0x6EA;
-    uintptr_t CurrentWeapon = 0x948;
+    uintptr_t CurrentWeapon = 0xa20;
     uintptr_t entity_actor = 0x310;
     uintptr_t GlobalAnimRateScale = 0xA80;
     uintptr_t LocalPlayer = 0xE9F9A60;
-    uintptr_t UWorld = 0xF16DE38;
-    uintptr_t ViewPoint = 0xE4A07E0;
-    uintptr_t Gameinstance = 0x1b8;
+    uintptr_t UWorld = 0x117392A8;
+    uintptr_t ViewPoint = 0xE49C7E0;
+    uintptr_t Gameinstance = 0x1d0;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
     uintptr_t PlayerCameraManager = 0x348;
@@ -25,7 +25,7 @@ namespace OFFSETS
     uintptr_t RootComponet = 0x198;
     uintptr_t RelativeLocation = 0x128;
     uintptr_t GameState = 0x158;
-    uintptr_t PlayerArray = 0x2a8;
+    uintptr_t PlayerArray = 0x2A8;
     uintptr_t WeaponData = 0x450;
     uintptr_t PawnPrivate = 0x308;
     uintptr_t TeamIndex = 0x10e0;
@@ -36,21 +36,21 @@ namespace OFFSETS
     uintptr_t FText = 0x38;
     uintptr_t Tier = 0x73;
     uintptr_t Mesh = 0x318;
-    uintptr_t LastSubmitTime = 0x368;
+    uintptr_t LastSubmitTime = 0x360;
     uintptr_t LastRenderTimeOnScreen = 0x370;
     uintptr_t bIsDying = 0x750;
-    uintptr_t isDBNO = 0x872;
-    uintptr_t BoneArray = 0x610;
-    uintptr_t BoneArrayCache = 0x648;
+    uintptr_t isDBNO = 0x9A3;
+    uintptr_t BoneArray = 0x608;
+    uintptr_t BoneArrayCache = 0x658;
     uintptr_t pNameStructure = 0xAE0;
-    uintptr_t ComponentToWorld = 0x240;
-    uintptr_t SkeletalMeshes = 0x56e;
-    uintptr_t PawnMaterials_ALL = 0x5A60;
-    uintptr_t Wireframe = 0x194;
+    uintptr_t ComponentToWorld = 0x230;
+    uintptr_t SkeletalMeshes = 0xb40;
+    uintptr_t PawnMaterials_ALL = 0x5b80;
+    uintptr_t Wireframe = 0x7ce;
     uintptr_t PersistentLevel = 0x30;
     uintptr_t ActorCount = 0xA0;
-    uintptr_t AActor = 0x98;
-    uintptr_t CurrentActor = 0x8;
+    uintptr_t AActor = 0xa0;
+    uintptr_t CurrentActor = 0xa8;
     uintptr_t TeamId = 0x10e0;
     uintptr_t Levels = 0x170;
     uintptr_t IsCached = 0x648;
